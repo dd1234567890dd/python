518ad847e8c9cca510570494c334d06c560b006f
danis_hayrullin <113259145+dd1234567890dd@users.noreply.github.com>
2022-11-14 21:08:35 +0300

    Add files via upload

---
+++ task_02.py
@@ -0,0 +1,8 @@
+
+city = ["Moscow", "Rio", "New York"]
+population = [
+    ["Moscow", 12.6],
+    ["Rio", 6.8],
+    ["New York", 8.3]
+]
+print('население '+ str(population[1][0]) + " - " + str(population[1][1]) + " миллионов человек")
