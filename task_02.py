5f7b1e1ba55f2e421e59c3e602351b09d5a6d7fa
Nikita Pyshkov <85805562+pyshkovni@users.noreply.github.com>
2022-11-18 23:13:22 +0300

    Добавил решения

--- task_02.py
+++ task_02.py
@@ -6,3 +6,21 @@ population = [
     ["New York", 8.3]
 ]
 print('население '+ str(population[1][0]) + " - " + str(population[1][1]) + " миллионов человек")
+
+# Хорошо!
+# Вот еще варианты
+
+# Решение 1 с функцией
+def total_sun(lst):
+    total = 0
+
+    for i in lst:
+        total += i[1]
+
+    return total
+
+# Решение 2 с функцией в одну строку
+def total_sum(lst):
+    return sum([i[1] for i in lst])
+
+print(total_sum(population))
