c2da0682ca26cddee3700ea2d6fe67638622fd88
danis_hayrullin <113259145+dd1234567890dd@users.noreply.github.com>
2022-11-30 00:49:45 +0300

    Add files via upload

---
+++ task_04.py
@@ -0,0 +1,28 @@
+# Задача 4
+# Приведем плейлист песен в виде списка списков
+# Список my_favorite_songs содержит список названий и длительности каждого трека
+# Выведите общее время звучания трех случайных песен в формате
+# Три песни звучат ХХХ минут
+# Для того, чтобы задавать случайные значения, используйсте модуль random
+
+import random
+
+mfs = [
+    ['Waste a Moment', 3.03],
+    ['New Salvation', 4.02],
+    ['Staying\' Alive', 3.40],
+    ['Out of Touch', 3.03],
+    ['A Sorta Fairytale', 5.28],
+    ['Easy', 4.15],
+    ['Beautiful Day', 4.04],
+    ['Nowhere to Run', 2.58],
+    ['In This World', 4.02],
+]
+
+mfs1 = []
+for i in sum(mfs, []):
+    if type(i) is float:
+       mfs1.append(i)
+
+t = sum(random.choices(mfs1, k=3))
+print("три песни звучат", t , "минут")
